b41f6872d242e162c4129a445e58f99bbe5feadd
shoma <shoma.h4a+github@gmail.com>
2011-06-15 14:44:52 +0900

    クラスの __doc__ 用にメタクラスを追加

--- dochelpers/functions.py
+++ dochelpers/functions.py
@@ -3,6 +3,79 @@
 import sys
 import os
 import inspect
+import types
+
+
+
+class _Dummy(object):
+    pass
+
+
+def get_args(tgt):
+    u'''
+    クラスか関数から引数名リストを返す
+    '''
+
+    def getargspec_from_method(meth):
+        u'''
+        self を抜いた argspec と同等の情報
+        '''
+        argspec = inspect.getargspec(meth)
+        tmp = _Dummy()
+        tmp.args = argspec.args[1:]
+        tmp.varargs = argspec.varargs
+        tmp.keywords = argspec.keywords
+        argspec = tmp
+
+        return tmp
+        
+    if isinstance(tgt, types.FunctionType):
+        argspec = inspect.getargspec(tgt)
+    elif isinstance(tgt, dict):
+        __init__ = tgt['__init__']
+        argspec = getargspec_from_method(__init__)
+        
+    elif hasattr(tgt, '__init__'):
+        argspec = getargspec_from_method(tgt.__init__)
+    else:
+        raise ValueError, 'unknown type: {0}'.format(tgt)
+
+    args = argspec.args
+
+    args = zip(args, args)
+
+    if argspec.varargs:
+        args.append((argspec.varargs, '*'+argspec.varargs))
+    if argspec.keywords:
+        args.append((argspec.keywords, '**'+argspec.keywords))
+
+    return args
+
+
+
+def paramdoc_meta(_retdoc, _rettype=None, **argd):
+    u'''
+    __init__ の引数情報を cls.__doc__ に追加するためのメタクラス
+
+    :param str _retdoc: 返り値の説明
+    :param str _rettype: 返り値の型
+    :param **argd: 引数名 -> (説明, 型) なキーワード引数群。説明だけでも可
+    :return: メタクラスとして使うためのオブジェクト
+    '''
+
+    f = paramdoc_generator(_retdoc, _rettype, **argd)
+
+    def meta(name, bases, attrs):
+        u'''
+        メタクラス同等品ですヨ
+        '''
+
+        attrs = f(attrs)
+
+        return type(name, bases, attrs)
+
+    return meta
+
 
 
 def paramdoc_generator(_retdoc, _rettype=None, **argd):
@@ -12,6 +85,7 @@ def paramdoc_generator(_retdoc, _rettype=None, **argd):
     :param str _retdoc: 返り値の説明
     :param str _rettype: 返り値の型
     :param **argd: 引数名 -> (説明, 型) なキーワード引数群。説明だけでも可
+    :return: デコレータ
     '''
 
     def decorator(f):
@@ -19,21 +93,14 @@ def paramdoc_generator(_retdoc, _rettype=None, **argd):
         デコレータですヨ
         '''
 
-        argspec = inspect.getargspec(f)
-
-        args = argspec.args
-
-        if argspec.varargs:
-            args.append(argspec.varargs)
-        if argspec.keywords:
-            args.append(argspec.keywords)
+        args = get_args(f)
 
         docs = []
 
-        for arg in args:
+        for (arg, name) in args:
             
             if arg not in argd:
-                docs.append(':param {0}:'.format(arg))
+                docs.append(':param {0}:'.format(name))
                 continue
 
             v = argd[arg]
@@ -43,10 +110,10 @@ def paramdoc_generator(_retdoc, _rettype=None, **argd):
             else:
                 desc, typ = v, None
 
-            docs.append(':param {0}: {1}'.format(arg, desc))
+            docs.append(':param {0}: {1}'.format(name, desc))
 
             if typ is not None:
-                docs.append(':type {0}: {1}'.format(arg, typ))
+                docs.append(':type {0}: {1}'.format(name, typ))
 
 
         docs.append(':return: {0}'.format(_retdoc))
@@ -54,10 +121,15 @@ def paramdoc_generator(_retdoc, _rettype=None, **argd):
         if _rettype is not None:
             docs.append(':rtype: {0}'.format(_rettype))
 
-        if f.__doc__ is None:
-            f.__doc__ = ''
 
-        f.__doc__ = f.__doc__ + '\n' + '\n'.join(docs)
+        if isinstance(f, dict):
+            f['__doc__'] = f.get('__doc__', '') + '\n' + '\n'.join(docs)
+
+        else:
+            if not hasattr(f, '__doc__') or f.__doc__ is None:
+                f.__doc__ = ''
+            f.__doc__ = f.__doc__ + '\n' + '\n'.join(docs)
+            
 
         return f
 
--- test/test_functions.py
+++ test/test_functions.py
@@ -1,11 +1,14 @@
 #-*- coding:utf-8 -*-
 
-
+import sys
 from dochelpers import functions
 
 
 
 def test_paramdoc_generator():
+    u'''
+    デコレータ形式
+    '''
 
     @functions.paramdoc_generator(u'何か返す', 'str',
                                   a=(u'あいうえお', 'int'),
@@ -17,3 +20,30 @@ def test_paramdoc_generator():
 
 
     assert len(test.__doc__) > 10
+
+
+
+def test_class():
+    u'''
+    メタクラス形式
+    '''
+
+
+    class test(object):
+        u'''テストですヨ'''
+
+        __metaclass__ = functions.paramdoc_meta(u'何か返す', 'str',
+                                                a=(u'あいうえお', 'int'),
+                                                b=u'かきくけこ',
+                                                argl=(u'かきくけこ', 'aaa'))
+        
+
+        def __init__(self, a, b, c, *argl, **argd):
+            pass
+
+
+    print >> sys.stderr, test.__doc__
+
+
+    assert len(test.__doc__) > 10
+
